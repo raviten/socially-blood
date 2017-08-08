095cc571f0d03ed6aec3f52d9a0492cd579fb943
ravi_teja_n <ravi@quantumgraph.com>
2017-08-08 10:07:46 +0530

    merged changes of Post form and storing fb creds on database

--- app/src/Containers/Forms/Connectors.js
+++ app/src/Containers/Forms/Connectors.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
-// import 'firebase/database';
+import 'firebase/database';
 // const firebaseui = require('firebaseui');
 const ls = require('local-storage');
 
@@ -10,7 +10,11 @@ export default class Connectors extends React.Component {
 
   constructor(props) {
     super(props);
-    let {googleConn, facebookConn, twitterConn} = this.getLoginStatus()
+    let {
+      googleConn,
+      facebookConn,
+      twitterConn
+    } = this.getLoginStatus()
     this.state = {
       googleConn: googleConn,
       facebookConn: facebookConn,
@@ -26,18 +30,49 @@ export default class Connectors extends React.Component {
     let googleConn = false,
       facebookConn = false,
       twitterConn = false;
-    if(ls.get('facebookAccess')) {
+    if (ls.get('facebookAccess')) {
       facebookConn = true;
     }
-    return {googleConn, facebookConn, twitterConn}
+    return {
+      googleConn,
+      facebookConn,
+      twitterConn
+    }
   }
 
   componentDidMount() {
-    this.nativeFB();
+    var config = {
+      apiKey: "AIzaSyDuEaisA0lm5lTKCMCbj02SPEXETxG9Qbw",
+      authDomain: "socially-blood.firebaseapp.com",
+      databaseURL: "https://socially-blood.firebaseio.com",
+      projectId: "socially-blood",
+      storageBucket: "socially-blood.appspot.com",
+      messagingSenderId: "150982221530"
+    };
+    firebase.initializeApp(config);
+
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //   if (user) {
+    //     console.log(user);
+    //     var db = firebase.database();
+    //     var ref = db.ref("users");
+    //     var usersRef = ref.child(userId);
+    //     console.log(userId, usersRef);
+    //     var d = {
+    //         date_of_birth: "June 23, 1912",
+    //         full_name: "Alan Turing"
+    //       };
+    //     console.log(d);
+    //     usersRef.set(d);
+    //     // User is signed in.
+    //   } else {
+    //     // No user is signed in.
+    //   }
+    // });
   }
 
   componentDidUpdate() {
-    
+
   }
 
   // facebookPopUp2() {
@@ -56,9 +91,9 @@ export default class Connectors extends React.Component {
 
   //     // queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
   //     // signInSuccessUrl: './second.html',
-      
+
   //     signInFlow: 'popup',
-      
+
   //     signInOptions: [
   //       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
   //     ],
@@ -68,59 +103,30 @@ export default class Connectors extends React.Component {
   //   ui.start('#firebaseui-auth-container', uiConfig);
   // }
 
-  nativeFB() {
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId            : '435244583535721',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v2.10'
-      });
-      FB.AppEvents.logPageView();
-    };
-
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-  }
-
   facebookLogin() {
+    let self = this;
+    // firebase.auth().signInWithRedirect(this.state.facebookProvider);
     firebase.auth().signInWithPopup(this.state.facebookProvider).then(function(result) {
-      console.log('here', result);
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      ls.set('facebookAccess', result);
-      this.setState({'facebookConn': true})
-      // ...
+      let userId = result.user.uid;
+      ls.set('facebookAccess', result.credential.accessToken);
+      firebase.database().ref('users/' + userId + '/creds/facebook').set({
+        'token': result.credential.accessToken
+      });
+      self.setState({
+        'facebookConn': true
+      });
     }).catch(function(error) {
       console.log('error', error);
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
     });
   }
 
   render() {
-    var config = {
-      apiKey: "AIzaSyDuEaisA0lm5lTKCMCbj02SPEXETxG9Qbw",
-      authDomain: "socially-blood.firebaseapp.com",
-      databaseURL: "https://socially-blood.firebaseio.com",
-      projectId: "socially-blood",
-      storageBucket: "socially-blood.appspot.com",
-      messagingSenderId: "150982221530"
-    };
-    firebase.initializeApp(config);
-
     return (
       <div>
         <RaisedButton label="Login with Facebook"
-            labelColor="#FFF" 
-            backgroundColor="#3b5998" 
+            labelColor="#FFF"
+            backgroundColor="#3b5998"
             onClick={this.facebookLogin.bind(this)}
             disabled={this.state.facebookConn}
         />
--- app/src/Containers/Forms/PostForm.js
+++ app/src/Containers/Forms/PostForm.js
@@ -1,22 +1,138 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/* Material UI imports*/
+import DropDownMenu from 'material-ui/DropDownMenu/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem/MenuItem';
+import TextField from "material-ui/TextField/TextField";
+import FlatButton from "material-ui/FlatButton/FlatButton";
+import RaisedButton from "material-ui/RaisedButton/RaisedButton";
+import SelectField from 'material-ui/SelectField';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+
+/* App based imports*/
 import Connectors from './Connectors';
 
 export default class MainLayout extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      bloodGroups: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+      selectedBloodGroup: "A+",
+      "mobile": "",
+      "error": false,
+      "errorMessageMobile": "",
+      "endTime": "6 hours",
+      "location": {}
+    }
+  }
+
+
+ changeBloodGroup(event, key, value) {
+    var selectedBloodGroup = value;
+    this.setState({selectedBloodGroup: selectedBloodGroup});
+  }
+
+  updateMobile(event) {
+    var mobile = event.target.value;
+    if(mobile > 9999999999){
+      return;
+    }
+    this.setState({mobile: mobile});
+  }
+
+  updateEndTime(event, key, value) {
+    this.setState({endTime: value});
+  }
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({location: position});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
+  submitForm() {
+    if (this.state.mobile == "") {
+      this.setState({errorMessageMobile: "This field is required"});
+      return;
+    } else if (this.state.mobile < 1000000000) {
+      this.setState({errorMessageMobile: "Invalid Mobile Number"});
+      return;
+    } else {
+      this.setState({errorMessageMobile: ""})
+    }
   }
 
   render() {
-
+    var endTimeArray = [];
+    for (var i=1; i<=24; i++) {
+      var value = i + " hours";
+      endTimeArray.push(<MenuItem key={i} value={value} primaryText={value} />);
+    }
+    var location = this.state.location;
     return (
       <div>
       <Connectors/>
-        <div>
-          This is awesome form!
+      <Card>
+        <CardHeader/>
+        <CardTitle> Please fill below form</CardTitle>
+        <div style={{marginLeft: 50}}>
+          <div style={{marginTop: 20}}>
+            <SelectField value={this.state.selectedBloodGroup}
+                          floatingLabelText="Your Blood Group"
+                          onChange={this.changeBloodGroup.bind(this)}>
+                {
+                  this.state.bloodGroups.map(function(blood, index) {
+                    return (<MenuItem key={index} value={blood} primaryText={blood} />)
+                  })
+                }
+            </SelectField>
+          </div>
+          <div>
+            <TextField hintStyle={{color: "#AAA"}}
+                    floatingLabelStyle={{color: "#AAA"}}
+                    value={this.state.mobile}
+                    errorText={this.state.errorMessageMobile}
+                    type="number"
+                    min={0}
+                    onChange={this.updateMobile.bind(this)}
+                    hintText="10 Digit Mobile Number*"
+                    floatingLabelText="Mobile Number*"
+            />
+          </div>
+          <div style={{marginTop: 20}}>
+              <SelectField value={this.state.endTime}
+                            floatingLabelText="Ends In:"
+                            onChange={this.updateEndTime.bind(this)}>
+                {endTimeArray}
+              </SelectField>
+
+          </div>
+          <div style={{marginTop: 20}}>
+            <RaisedButton label="Set Location" onClick={this.getLocation.bind(this)} />
+            { location.coords
+              ? <div style={{marginTop: 20}}>
+                  <div>Latitude: {location.coords.latitude}</div>
+                  <div>Longitude: {location.coords.longitude}</div>
+                </div>
+              : ""
+            }
+          </div>
+           <CardActions style={{marginLeft: 100, marginTop: 100}}>
+            <RaisedButton label="Submit"
+                    backgroundColor="#32CD32"
+                    labelColor="#FFF"
+                    style={{marginBottom: 100}}
+                    onClick={this.submitForm.bind(this)}/>
+          </CardActions>
         </div>
+      </Card>
       </div>
     );
   }
